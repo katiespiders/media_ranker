c347d1d22be87f7ad7d7203222a6d6247fd51226
Katie <kab761@gmail.com>
2014-10-28 14:50:40 -0700

    streamlined partials

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -1,24 +1,37 @@
 class MediaController < ApplicationController
+  before_action :set_type
 
   def index
     @media = type_class.all
   end
 
   def show
+    set_attribution
     @medium = type_class.find(params[:id])
   end
 
-  private
-    def set_type
-      @type = params[:type]
-    end
+  def new
 
-    def type
-      Medium.types.include?(params[:type]) ? params[:type] : "Medium"
+  end
+
+  private
+
+    def set_type
+      @type = params[:type] ? params[:type] : "Medium"
     end
 
     def type_class
-      type.constantize # looks for a declared constant with name type
+      @type.constantize # looks for a declared constant with name type
     end
+
+    def set_attribution
+      @attribution = case @type
+        when "Book" then "Author: "
+        when "Movie" then "Director: "
+        when "Album" then "Artist: "
+        else "Creator: "
+      end
+    end
+
 
 end
