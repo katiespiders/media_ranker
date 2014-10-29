610d7930184fb84937a1e8c87703923bdda48f66
Katie <kab761@gmail.com>
2014-10-29 16:11:46 -0700

    implemented sorting by upvotes

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -1,6 +1,7 @@
 class Medium < ActiveRecord::Base
 
   def self.types
-    %w(Album Book Movie) # list of all types of Medium (classes that inherit from it) as strings; type.constantize (in MediaController) will look for classes with these names. look up %w syntax
+    %w(Album Book Movie) # list of all types of Medium (classes that inherit from it) as strings; type.constantize (in MediaController) will look for classes with these names.
+    #to do: look up %w syntax
   end
 end
