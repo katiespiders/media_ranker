772adbf96a9b1619d668496d37c3a515f435726a
Katie <kab761@gmail.com>
2014-10-28 17:11:11 -0700

    made new form and create method

--- app/controllers/media_controller.rb
+++ app/controllers/media_controller.rb
@@ -11,7 +11,16 @@ class MediaController < ApplicationController
   end
 
   def new
+    set_attribution
+  end
 
+  def create
+    @medium = type_class.new(medium_params)
+    if @medium.save
+      redirect_to "/#{@type.downcase.pluralize}"
+    else
+      render :new
+    end
   end
 
   private
@@ -25,11 +34,15 @@ class MediaController < ApplicationController
     end
 
     def set_attribution
-      @attribution = case @medium.type
+      @attribution = case @type
         when "Book" then "Author: "
         when "Movie" then "Director: "
         when "Album" then "Artist: "
         else "Creator: "
       end
+    end
+
+    def medium_params
+      params.require(:media).permit(:title, :author, :description)
     end
 end
